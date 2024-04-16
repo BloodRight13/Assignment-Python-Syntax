6896b773845b1bdcc5e438299fab48546c459ef4
BloodRight13 <166233088+BloodRight13@users.noreply.github.com>
2024-04-16 13:15:23 -0500

    first commit

--- 3. Python Data Types and type() Function.py
+++ 3. Python Data Types and type() Function.py
@@ -1,13 +1,9 @@
 #Task 1 Code Correction
 variable_a = "Hello, World!" #string
-type (variable_a)
 print(variable_a, type(variable_a))
 variable_b = 23 #interger
-type(variable_b)
 print(variable_b, type(variable_b))
 variable_c = 3.14 #float
-type (variable_c)
 print(variable_c, type(variable_c))
 variable_d = True #bool
-type(variable_d)
 print(variable_d, type(variable_d))
